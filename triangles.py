51732974ceec7dfedbc97dd8e61e3e9b17d4beba
MrSprint <18240069963@163.com>
2016-10-28 13:38:06 +0800

    Update triangles.py

--- triangles.py
+++ triangles.py
@@ -21,12 +21,12 @@ for t in triangles():
         
         
         
-““”
+"""
 def triangles():
     L = [1]
     while True:
         yield L
         L.append(0);
         L = [L[i-1] + L[i] for i in range(len(L))]
-        “”“
+     """
 #杨辉三角的另一种解法
